a788dbef92624bdf74c34c296129d75638aad98f
vblago <vblago.ltd@gmail.com>
2019-04-23 14:06:22 +0300

    constants refactoring v.0.2.1

--- ios/Classes/SwiftFlutterKinEcosystemSdkPlugin.swift
+++ ios/Classes/SwiftFlutterKinEcosystemSdkPlugin.swift
@@ -11,9 +11,9 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
     static let infoFlutterController = FlutterStreamController()
     
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_kin_ecosystem_sdk", binaryMessenger: registrar.messenger())
-        let balanceEventChannel = FlutterEventChannel.init(name: "flutter_kin_ecosystem_sdk_balance", binaryMessenger: registrar.messenger())
-        let infoEventChannel = FlutterEventChannel.init(name: "flutter_kin_ecosystem_sdk_info", binaryMessenger: registrar.messenger())
+        let channel = FlutterMethodChannel(name: Constants.FLUTTER_KIN_ECOSYSTEM_SDK.rawValue, binaryMessenger: registrar.messenger())
+        let balanceEventChannel = FlutterEventChannel.init(name: Constants.FLUTTER_KIN_ECOSYSTEM_SDK_BALANCE.rawValue, binaryMessenger: registrar.messenger())
+        let infoEventChannel = FlutterEventChannel.init(name: Constants.FLUTTER_KIN_ECOSYSTEM_SDK_INFO.rawValue, binaryMessenger: registrar.messenger())
         let instance = SwiftFlutterKinEcosystemSdkPlugin()
         balanceEventChannel.setStreamHandler(balanceFlutterController)
         infoEventChannel.setStreamHandler(infoFlutterController)
@@ -21,7 +21,7 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        if(call.method.elementsEqual("kinStart")){
+        if(call.method.elementsEqual(Constants.KIN_START.rawValue)){
             let arguments = call.arguments as? NSDictionary
             let token = arguments!["token"] as? String
             let userId = arguments!["userId"] as? String
@@ -38,43 +38,43 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
             Kin.shared.migrationDelegate = self
             do {
                 try Kin.shared.start(userId: userId!, appId: appId!, jwt: token, environment: environment)
-                sendReport(type: "kinStart", message: "Kin started")
+                sendReport(type: Constants.KIN_START.rawValue, message: "Kin started")
                 isKinInit = true
                 initializeBalanceObserver(initBalanceObserver: initBalanceObserver!)
             } catch {
                 isKinInit = false
-                sendError(type: "kinStart", error: error)
+                sendError(type: Constants.KIN_START.rawValue, error: error)
             }
         }
-        if(call.method.elementsEqual("launchKinMarket")){
+        if(call.method.elementsEqual(Constants.LAUNCH_KIN_MARKET.rawValue)){
             if (!ifKinInit()) {return}
             let viewController = (UIApplication.shared.delegate?.window??.rootViewController)!;
             Kin.shared.launchMarketplace(from: viewController)
         }
-        if(call.method.elementsEqual("getWallet")){
+        if(call.method.elementsEqual(Constants.GET_WALLET.rawValue)){
             if (!ifKinInit()) {return}
             result(Kin.shared.publicAddress)
         }
-        if(call.method.elementsEqual("kinEarn")){
+        if(call.method.elementsEqual(Constants.KIN_EARN.rawValue)){
             if (!ifKinInit()) {return}
             let arguments = call.arguments as? NSDictionary
             let jwt = arguments!["jwt"] as? String
             kinEarn(jwt: jwt!)
         }
-        if(call.method.elementsEqual("kinSpend")){
+        if(call.method.elementsEqual(Constants.KIN_SPEND.rawValue)){
             if (!ifKinInit()) {return}
             let arguments = call.arguments as? NSDictionary
             let jwt = arguments!["jwt"] as? String
             kinSpend(jwt: jwt!)
         }
-        if(call.method.elementsEqual("kinPayToUser")){
+        if(call.method.elementsEqual(Constants.KIN_PAY_TO_USER.rawValue)){
             if (!ifKinInit()) {return}
             let arguments = call.arguments as? NSDictionary
             let jwt = arguments!["jwt"] as? String
             kinPayToUser(jwt: jwt!)
         }
-        if(call.method.elementsEqual("orderConfirmation")){
-            let err = PluginError(type: "orderConfirmation", message: "Kin SDK iOS doesn't support function orderConfirmation.")
+        if(call.method.elementsEqual(Constants.ORDER_CONFIRMATION.rawValue)){
+            let err = PluginError(type: Constants.ORDER_CONFIRMATION.rawValue, message: "Kin SDK iOS doesn't support function orderConfirmation.")
             sendError(code: "-2", message: "Kin SDK iOS doesn't support function orderConfirmation.", details: err)
         }
     }
@@ -87,7 +87,7 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
                     SwiftFlutterKinEcosystemSdkPlugin.balanceFlutterController.eventCallback?(self.currentBalance)
                 }
             } catch {
-                self.sendError(type: "balanceObserver", error: error)
+                self.sendError(type: Constants.BALANCE_OBSERVER.rawValue, error: error)
             }
         }
     }
@@ -96,9 +96,9 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
         let prevBalance = currentBalance
         let handler: ExternalOfferCallback = { jwtConfirmation, error in
             if jwtConfirmation != nil {
-                self.sendReport(type: "kinEarn", message: String(describing: jwtConfirmation), amount: self.currentBalance - prevBalance)
+                self.sendReport(type: Constants.KIN_EARN.rawValue, message: String(describing: jwtConfirmation), amount: self.currentBalance - prevBalance)
             } else if let e = error {
-                self.sendError(type: "kinEarn", error: e)
+                self.sendError(type: Constants.KIN_EARN.rawValue, error: e)
             }
         }
         _ = Kin.shared.requestPayment(offerJWT: jwt, completion: handler)
@@ -107,9 +107,9 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
     private func kinSpend(jwt : String){
         _ = Kin.shared.purchase(offerJWT: jwt) { jwtConfirmation, error in
             if jwtConfirmation != nil {
-                self.sendReport(type: "kinSpend", message: String(describing: jwtConfirmation))
+                self.sendReport(type: Constants.KIN_SPEND.rawValue, message: String(describing: jwtConfirmation))
             } else if let e = error {
-                self.sendError(type: "kinSpend", error: e)
+                self.sendError(type: Constants.KIN_SPEND.rawValue, error: e)
             }
         }
     }
@@ -117,9 +117,9 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
     private func kinPayToUser(jwt : String){
         _ = Kin.shared.payToUser(offerJWT: jwt) { jwtConfirmation, error in
             if jwtConfirmation != nil {
-                self.sendReport(type: "kinPayToUser", message: String(describing: jwtConfirmation))
+                self.sendReport(type: Constants.KIN_PAY_TO_USER.rawValue, message: String(describing: jwtConfirmation))
             } else if let e = error {
-                self.sendReport(type: "kinPayToUser", message: String(describing: e))
+                self.sendReport(type: Constants.KIN_PAY_TO_USER.rawValue, message: String(describing: e))
             }
         }
     }
@@ -182,7 +182,7 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
     
     private func ifKinInit() -> Bool{
         if(!isKinInit){
-            let err = PluginError(type: "kinStart", message: "Kin SDK not started")
+            let err = PluginError(type: Constants.KIN_START.rawValue, message: "Kin SDK not started")
             sendError(code: "-1", message: "Kin SDK not started", details: err)
         }
         return isKinInit
@@ -207,20 +207,35 @@ public class SwiftFlutterKinEcosystemSdkPlugin: NSObject, FlutterPlugin {
 
 extension SwiftFlutterKinEcosystemSdkPlugin: KinMigrationDelegate {
     public func kinMigrationDidStart() {
-        sendReport(type: "kinMigration", message: "migrationStart")
+        sendReport(type: Constants.KIN_MIGRATION.rawValue, message: "migrationStart")
     }
     
     public func kinMigrationDidFinish() {
-        sendReport(type: "kinMigration", message: "migrationFinish")
+        sendReport(type: Constants.KIN_MIGRATION.rawValue, message: "migrationFinish")
         initializeBalanceObserver(initBalanceObserver: initBalanceObserver!)
     }
     
     public func kinMigrationIsReady() {
-        sendReport(type: "kinMigration", message: "migrationReady")
+        sendReport(type: Constants.KIN_MIGRATION.rawValue, message: "migrationReady")
     }
     
     public func kinMigration(error: Error) {
-        let err = PluginError(type: "kinMigration", message: error.localizedDescription)
+        let err = PluginError(type: Constants.KIN_MIGRATION.rawValue, message: error.localizedDescription)
         sendError(code: "-2", message: "Migration failed", details: err)
+    }
+    
+    enum Constants: String {
+        case FLUTTER_KIN_ECOSYSTEM_SDK = "flutter_kin_ecosystem_sdk"
+        case FLUTTER_KIN_ECOSYSTEM_SDK_BALANCE = "flutter_kin_ecosystem_sdk_balance"
+        case FLUTTER_KIN_ECOSYSTEM_SDK_INFO = "flutter_kin_ecosystem_sdk_info"
+        case KIN_START = "KinStart"
+        case LAUNCH_KIN_MARKET = "LaunchKinMarket"
+        case GET_WALLET = "GetWallet"
+        case KIN_EARN = "KinEarn"
+        case KIN_SPEND = "KinSpend"
+        case KIN_PAY_TO_USER = "KinPayToUser"
+        case KIN_MIGRATION = "KinMigration"
+        case ORDER_CONFIRMATION = "OrderConfirmation"
+        case BALANCE_OBSERVER = "BalanceObserver"
     }
 }
--- lib/flutter_kin_ecosystem_sdk.dart
+++ lib/flutter_kin_ecosystem_sdk.dart
@@ -6,12 +6,12 @@ import 'package:flutter_kin_ecosystem_sdk/utils.dart';
 
 class FlutterKinEcosystemSdk {
   static MethodChannel _methodChannel =
-      MethodChannel('flutter_kin_ecosystem_sdk');
+      MethodChannel(FlutterKinEcosystemSDKConstans.FLUTTER_KIN_ECOSYSTEM_SDK);
 
-  static const _streamBalance =
-      const EventChannel('flutter_kin_ecosystem_sdk_balance');
-  static const _streamInfo =
-      const EventChannel('flutter_kin_ecosystem_sdk_info');
+  static const _streamBalance = const EventChannel(
+      FlutterKinEcosystemSDKConstans.FLUTTER_KIN_ECOSYSTEM_SDK_BALANCE);
+  static const _streamInfo = const EventChannel(
+      FlutterKinEcosystemSDKConstans.FLUTTER_KIN_ECOSYSTEM_SDK_INFO);
 
   static StreamController<Info> _streamInfoController =
       new StreamController.broadcast();
@@ -37,8 +37,8 @@ class FlutterKinEcosystemSdk {
   }
 
   static Future<String> get getPublicAddress async {
-    final String address =
-        await _methodChannel.invokeMethod('getPublicAddress');
+    final String address = await _methodChannel
+        .invokeMethod(FlutterKinEcosystemSDKConstans.GET_WALLET);
     return address;
   }
 
@@ -60,38 +60,62 @@ class FlutterKinEcosystemSdk {
       'initBalanceObserver': initBalanceObserver,
       'isProduction': isProduction,
     };
-    await _methodChannel.invokeMethod('kinStart', params);
+    await _methodChannel.invokeMethod(
+        FlutterKinEcosystemSDKConstans.KIN_START, params);
   }
 
   static Future launchKinMarket() async {
-    await _methodChannel.invokeMethod('launchKinMarket');
+    await _methodChannel
+        .invokeMethod(FlutterKinEcosystemSDKConstans.LAUNCH_KIN_MARKET);
   }
 
   static Future kinEarn(String jwt) async {
     final Map<String, dynamic> params = <String, dynamic>{
       'jwt': jwt,
     };
-    await _methodChannel.invokeMethod('kinEarn', params);
+    await _methodChannel.invokeMethod(
+        FlutterKinEcosystemSDKConstans.KIN_EARN, params);
   }
 
   static Future kinSpend(String jwt) async {
     final Map<String, dynamic> params = <String, dynamic>{
       'jwt': jwt,
     };
-    await _methodChannel.invokeMethod('kinSpend', params);
+    await _methodChannel.invokeMethod(
+        FlutterKinEcosystemSDKConstans.KIN_SPEND, params);
   }
 
   static Future kinPayToUser(String jwt) async {
     final Map<String, dynamic> params = <String, dynamic>{
       'jwt': jwt,
     };
-    await _methodChannel.invokeMethod('kinPayToUser', params);
+    await _methodChannel.invokeMethod(
+        FlutterKinEcosystemSDKConstans.KIN_PAY_TO_USER, params);
   }
 
   static Future orderConfirmation(String offerId) async {
     final Map<String, dynamic> params = <String, dynamic>{
       'offerId': offerId,
     };
-    await _methodChannel.invokeMethod('orderConfirmation', params);
+    await _methodChannel.invokeMethod(
+        FlutterKinEcosystemSDKConstans.ORDER_CONFIRMATION, params);
   }
+}
+
+class FlutterKinEcosystemSDKConstans {
+  static const String FLUTTER_KIN_SDK = 'flutter_kin_sdk';
+  static const String FLUTTER_KIN_ECOSYSTEM_SDK = "flutter_kin_ecosystem_sdk";
+  static const String FLUTTER_KIN_ECOSYSTEM_SDK_BALANCE =
+      "flutter_kin_ecosystem_sdk_balance";
+  static const String FLUTTER_KIN_ECOSYSTEM_SDK_INFO =
+      "flutter_kin_ecosystem_sdk_info";
+  static const String KIN_START = "KinStart";
+  static const String LAUNCH_KIN_MARKET = "LaunchKinMarket";
+  static const String GET_WALLET = "GetWallet";
+  static const String KIN_EARN = "KinEarn";
+  static const String KIN_SPEND = "KinSpend";
+  static const String KIN_PAY_TO_USER = "KinPayToUser";
+  static const String KIN_MIGRATION = "KinMigration";
+  static const String ORDER_CONFIRMATION = "OrderConfirmation";
+  static const String BALANCE_OBSERVER = "BalanceObserver";
 }
